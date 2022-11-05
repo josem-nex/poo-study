4e6b28e3db23fc13d89199b64fa8d3a4a5bb79e7
josem-nex <josem.zayas@estudiantes.matcom.uh.cu>
2022-11-05 15:25:27 -0400

    second

--- Program.cs
+++ Program.cs
@@ -1,64 +1,23 @@
 using StudyPOO;
 namespace Tester;
-public class MyStopwatch{
-    long arrancada;
-    long parada;
-    bool andando;
-    public MyStopwatch(){
-        arrancada=0;
-        parada=0;
-        andando=false;
-    }
-    public bool Andando{ get; private set; }
-    public long Arrancada{ get; set; }
-    public long ElapsedMilliseconds{
-        get{
-            if(andando) return Environment.TickCount64 - arrancada;
-            else return parada-arrancada;
-        } 
-    }
-    public void Restart(){
-        this.arrancada=0;
-        this.parada=0;
-        this.andando= true;
-
-    }
-    public void Stop(){
-        this.parada= Environment.TickCount64;
-        this.andando = false;
-    }
-    public void Start(){
-        this.arrancada = Environment.TickCount64;
-        this.andando = true;
-    }
-}
 class Study
 {
     public static void Main()
     {
-        Cuenta personal = new Cuenta("Miguel", 0);
+        Cuenta personal = new Cuenta("Personal", 0);
         CuentaTransferencia familia = new CuentaTransferencia("Familia", 2000);
         familia.Transfiere(1000,personal);
         System.Console.WriteLine(personal.Saldo);
         System.Console.WriteLine(familia.Saldo);
         System.Console.WriteLine();
-        CuentaCredito probando = new CuentaCredito("probando", 500);
-        probando.Extraer(200);
-        System.Console.WriteLine(probando.Saldo);
-        probando.Extraer(1000);
-        System.Console.WriteLine(probando.Saldo);
+        CuentaCredito test = new CuentaCredito("test", 500);
+        test.Extraer(200);
+        System.Console.WriteLine(test.Saldo);
+        test.Extraer(1000);
+        System.Console.WriteLine(test.Saldo);
 
         
-        /* 
-        MyStopwatch a = new MyStopwatch();
-        Stopwatch crono = new Stopwatch();
-        a.Start();
-        crono.Start();
-        System.Console.WriteLine();
-        a.Stop();
-        crono.Stop();
-        System.Console.WriteLine(a.ElapsedMilliseconds);
-        System.Console.WriteLine(crono.ElapsedMilliseconds); */
+        
 
 
     }
